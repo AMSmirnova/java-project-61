f09372cdca52262212d29415a76390a19b796ba4
Anastasiia Smirnova <nyakubtsova@gmail.com>
2023-08-13 15:30:25 +0300

    fix games

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -18,7 +18,7 @@ public class Engine {
 
     public static void greetings() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\nWelcome to the Brain Games!");
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
